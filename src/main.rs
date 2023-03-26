913a41506391fe962fc219778d12f8ab6012b322
Fredrik Enestad <fredrik@enestad.com>
2023-03-26 11:43:58 +0200

    configurable port

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ use axum::{response::Html, routing::get, Router};
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(hello));
-    let addr = ([0, 0, 0, 0], 8080).into();
+    let addr = (
+        [0, 0, 0, 0],
+        std::env::var("PORT")
+            .map(|p| p.parse().unwrap())
+            .unwrap_or(8080),
+    )
+        .into();
 
     println!("starting the server");
     axum::Server::bind(&addr)
