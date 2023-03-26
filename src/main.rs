0fe0d590f2d39c3dea3c91e4c091925c99cc8885
Fredrik Enestad <fredrik@enestad.com>
2023-03-26 14:29:23 +0200

    setup basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use axum::{response::Html, routing::get, Router};
+use tracing::{dispatcher, metadata::LevelFilter, Dispatch};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() {
+    // Setup tracing subscriber with logfmt logging, and ability to set log level via RUST_LOG
+    let subscriber = Registry::default().with(tracing_logfmt::layer()).with(
+        EnvFilter::builder()
+            .with_default_directive(LevelFilter::INFO.into())
+            .from_env_lossy(),
+    );
+
+    dispatcher::set_global_default(Dispatch::new(subscriber))
+        .expect("failed setting up tracing dispatcher");
+
+    // Setup server and handlers
     let app = Router::new().route("/", get(hello));
     let addr = (
         [0, 0, 0, 0],
@@ -11,13 +24,13 @@ async fn main() {
     )
         .into();
 
-    println!("starting the server");
+    tracing::info!("starting the server, listening to {addr}");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .expect("failed starting the server");
 
-    println!("server stopped");
+    tracing::info!("server stopped");
 }
 
 async fn hello() -> Html<&'static str> {
